a35ff468ed376c80808da26a4795a084fafaa60b
Logitao <gmdramos@gmail.com>
2018-08-26 16:03:58 -0300

    [css] index.scss -> index.css

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-import './public/index.scss';
+import './public/index.css';
 import { HashRouter } from 'react-router-dom';
 import { Route } from 'react-router';
 import App from './components/App';
