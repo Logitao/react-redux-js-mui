76222a5ed7bc32065e5d8cafe5bd32556f065978
Logitao <gmdramos@gmail.com>
2018-08-28 13:11:11 -0300

    [redux] - template reducer e action completa

--- src/todos/todos.reducer.js
+++ src/todos/todos.reducer.js
@@ -1,16 +1,20 @@
-import { ADD_TODO } from './todos.actions';
+import { ADD_TODO, DELETE_TODO } from './todos.actions';
+import _ from 'lodash';
 
 const initialState = {
   todoItems: [
     {
+      key: 1,
       label: 'todo1',
       done: false
     },
     {
+      key: 2,
       label: 'todo2',
       done: false
     },
     {
+      key: 3,
       label: 'todo3',
       done: true
     }
@@ -20,8 +24,21 @@ const initialState = {
 export default (state = initialState, action) => {
   switch (action.type) {
     case ADD_TODO: {
-      console.log(action.payload.todo);
-      return { ...state, todoItems: [...state.todoItems, action.payload.todo] };
+      return {
+        ...state,
+        key: action.payload.todo.key,
+        todoItems: [...state.todoItems, action.payload.todo]
+      };
+    }
+    case DELETE_TODO: {
+      const { todoItems } = state;
+
+      console.log(state);
+
+      return {
+        ...state,
+        todoItems: _.dropRight(todoItems, 1)
+      };
     }
     default:
       return state;
