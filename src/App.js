578a126b756302aeb854295102369949dca132b1
Logitao <gmdramos@gmail.com>
2018-08-31 00:12:25 -0300

    [padrões] - correção nome de componente

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import React, { Component } from 'react';
-import TodoComponent from './todos/todos.component';
+import Todo from './todos/Todo';
 import { withRouter } from 'react-router';
 class App extends Component {
   render() {
     return (
       <div>
         <h1>Hello Webpack</h1>
-        <TodoComponent />
+        <Todo />
       </div>
     );
   }
